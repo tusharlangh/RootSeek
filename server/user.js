41aa84fcd505d60f26d585fa08f80b30c0f48c72
Tushar Langhnoda <tusharlanghnoda@gmail.com>
2025-03-21 14:35:47 -0600

    the user can now update their username, name, email and password

--- server/user.js
+++ server/user.js
@@ -34,6 +34,7 @@ const userSchema = new mongoose.Schema({
     password: {type: String, required: true},
     verificationCode: { type: String },
     verified: { type: Boolean, default: false },
+    tempEmail: {type: String, default: ""}
 })
 
 const User = mongoose.model("User", userSchema)
@@ -133,6 +134,76 @@ router.patch("/user/updateinformation", auth, async (req, res) => {
     }
 })
 
+router.post("/user/updateEmail", auth, async (req, res) => {
+    try {
+        const {newEmail} = req.body
+        if (!req.userId || !newEmail) return res.status(400).json({message:"Userid or the sent data is not valid."})
+
+        const existingUser = await User.findById(req.userId);
+        if (!existingUser) {
+            return res.status(404).json({ message: "User not found." });
+        }
+        if (existingUser.email === newEmail) {
+            return res.status(400).json({message:"Email already exists. Enter a different email."})
+        }
+
+        const verificationCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+        await User.findByIdAndUpdate(req.userId, {tempEmail: newEmail, verificationCode, verified: false})
+    
+        await sendVerificationEmail(newEmail, verificationCode)
+
+        res.status(201).json({message: "User information updated."})
+    } catch (error) {
+        res.status(500).json({message:"Server had a problem"})
+    }
+})
+
+router.post("/user/verify-email",auth, async (req,res) => {
+    try {
+        let {email, verificationCode} = req.body
+        email = email.toLowerCase();
+
+        const user = await User.findById(req.userId);
+        
+        if (!user) return res.status(400).json({message: "user not found."})
+
+        if (user.tempEmail === "") return res.status(400).json({ message: "No email update requested." });
+            
+        if (String(user.verificationCode) !== String(verificationCode)) {
+            user.tempEmail = ""
+            await user.save()
+            return res.status(400).json({message:"Invalid verification code."})
+        }
+
+        user.email = user.tempEmail
+        user.verificationCode = null
+        user.verified = true
+        user.tempEmail = ""
+
+        await user.save();
+    
+        res.json({message : "Email successfully verified!"})
+    } catch (error) {
+        res.status(500).json({message:"Server error"})
+    }
+})
+
+router.patch("/user/updatePassword", auth, async (req,res) => {
+    try {
+        const { password } = req.body
+        const user = await User.findById(req.userId)
+        if (!user) return res.status(400).json({message: "user not found."})
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        user.password = hashedPassword
+        await user.save();
+        res.status(200).json({ message: "Successfully updated password" })
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred." });
+    }
+})
 
 
 export default router
